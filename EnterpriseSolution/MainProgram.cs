93271b0d13bfe3fc9991a97335bc095a8b5f87e1
Chris_ngu <Chris_ngu@pm.me>
2019-12-19 16:32:56 -0600

    Implementing main application UI

--- EnterpriseSolution/MainProgram.cs
+++ EnterpriseSolution/MainProgram.cs
@@ -12,9 +12,33 @@ namespace EnterpriseSolution
 {
     public partial class MainProgram : Form
     {
+
         public MainProgram()
         {
             InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+        private void MainProgram_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+        Point lastPoint;
+        private void MainProgram_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
