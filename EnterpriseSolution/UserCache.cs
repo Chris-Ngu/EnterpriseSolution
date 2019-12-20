da7d00928f63131a2e43523c31a631df7307a07f
Chris_ngu <Chris_ngu@pm.me>
2019-12-20 15:28:53 -0600

    Implementing cache system

---
+++ EnterpriseSolution/UserCache.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseSolution.Forms
+{
+    public static class UserCache
+    {
+        //Do not store password in cache, no need to and big security problem
+        private static string username { get; set; }
+        private static string email { get; set; }
+        private static string personalNotes { get; set; }
+
+    }
+}
