077515555b72b5630e40dc2a704843e543cf01e4
Chris_ngu <Chris_ngu@pm.me>
2019-12-23 17:06:28 -0600

    Reverting google calendar integration

--- EnterpriseSolution/Program.cs
+++ EnterpriseSolution/Program.cs
@@ -3,39 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Google.Apis.Discovery.v1;
-using Google.Apis.Discovery.v1.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
 using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System.IO;
+using System.Text;
+using System.Threading;
 
 namespace EnterpriseSolution
 {
     class Program
     {
+
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Startup());
         }
 
-        private async Task Run()
-        {
-            var service = new DiscoveryService(new BaseClientService.Initializer
-            {
-                ApplicationName = "updateappointments",
-                ApiKey = "ENTER KEY HERE"
-            });
 
-            Console.Write("Executing a list request...");
-            var result = await service.Apis.List().ExecuteAsync();
-            if (result.Items != null)
-            {
-                foreach(Google.Apis.Discovery.v1.Data.DirectoryList.ItemsData api in result.Items)
-                {
-                    Console.WriteLine(api.Id + " - " + api.Title);
-                }
-            }
-        }
     }
 }
